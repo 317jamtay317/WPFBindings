6d951502f4623e7b75d515126e81bac984df9afc
317jamtay317 <jamtay317@gmail.com>
2023-11-26 06:42:03 -0500

    working

---
+++ DemoBindings/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using DemoBindings.Mvvm;
+using Prism.Mvvm;
+
+namespace DemoBindings;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public string OneWay
+    {
+        get => GetValue<string>();
+        set => SetValue(value);
+    }
+
+    public string TwoWay
+    {
+        get => GetValue<string>();
+        set => SetValue(value);
+    }
+
+    public string OneWayToSource
+    {
+        get => GetValue<string>();
+        set => SetValue(value);
+    }
+}
---
+++ DemoBindings/Mvvm/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.CompilerServices;
+using Prism.Mvvm;
+
+namespace DemoBindings.Mvvm;
+
+public abstract class ViewModelBase : BindableBase
+{
+    protected T GetValue<T>([CallerMemberName] string propertyName = null)
+    {
+        if (_values.ContainsKey(propertyName))
+        {
+            return (T)_values[propertyName];
+        }
+
+        return default(T);
+    }
+
+    protected void SetValue<T>(T value, [CallerMemberName] string propertyName = null)
+    {
+        _values[propertyName] = value;
+        RaisePropertyChanged(propertyName);
+    }
+    private Dictionary<string, object> _values = new();
+}
